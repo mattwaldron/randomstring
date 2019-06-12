faf137a66fa097cc31b47f06089508b8a9ae63fb
Matt Waldron <waldron.matthew@gmail.com>
2019-06-12 08:58:36 -0500

    initial commit

---
+++ RandomString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string charSet = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            var r = new Random();
+            var s = "";
+            var length = r.Next(8, 13);
+            while (length-- > 0)
+            {
+                s += charSet[r.Next(charSet.Length)];
+            }
+            Console.WriteLine($"Your random string is: {s}");
+        }
+    }
+}
